42f59623f07e86ec3dc19db0ee55b436c68c7d39
Alex Shannon <alexs1305@gmail.com>
2019-05-15 20:38:23 +0100

    java-questions: abstractvsinterface
    add abstract vs interface questions

--- src/questions/JavaQuestions.tsx
+++ src/questions/JavaQuestions.tsx
@@ -10,6 +10,28 @@ const data: QuestionType[] = [
     question: "Which map is sorted by default",
     answers: ["TreeMap", "HashMap"],
     correctAnswers: [0]
+  },
+  {
+    question:
+      "What are the similarities between abstract classes and interfaces",
+    answers: [
+      "They both allow private variables",
+      "they both define abstract methods",
+      "they both are used by defining the 'inherits' keyword",
+      "they both allow final variables",
+      "neither allow for instance methods"
+    ],
+    correctAnswers: [1, 3]
+  },
+  {
+    question: "What are the differences of abstract classes and interfaces",
+    answers: [
+      "abstract classes allow for instance methods",
+      "interfaces allow for only final variables and these are public",
+      "interfaces are 'implments' whereas abstract are 'extends'",
+      "abstract classes can have implemented methods, interfaces can only have abstracted methods"
+    ],
+    correctAnswers: [0, 1, 2, 3]
   }
 ];
 
